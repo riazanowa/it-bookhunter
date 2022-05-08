5aff27839be9a4b57d273e241618662fbdeee586
Ekaterina Ryazanova <kat.riazanowa@yandex.ru>
2022-05-08 06:21:43 +0300

    search for products by category

--- backend/it-bookstore-spring-boot/src/main/java/ru/ryazanova/itbookstore/config/MyDataRestConfig.java
+++ backend/it-bookstore-spring-boot/src/main/java/ru/ryazanova/itbookstore/config/MyDataRestConfig.java
@@ -1,5 +1,6 @@
 package ru.ryazanova.itbookstore.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -8,8 +9,22 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import ru.ryazanova.itbookstore.entity.Product;
 import ru.ryazanova.itbookstore.entity.ProductCategory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         HttpMethod[] unsupportedActions = {HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST};
@@ -25,5 +40,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+
+        //call internal helper method
+        exposeIds(config);
     }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+        //get a list of all entity classes from entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        //create array of entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        //- get the entity types for entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity / domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
 }
--- backend/it-bookstore-spring-boot/src/main/java/ru/ryazanova/itbookstore/dao/ProductRepository.java
+++ backend/it-bookstore-spring-boot/src/main/java/ru/ryazanova/itbookstore/dao/ProductRepository.java
@@ -1,11 +1,19 @@
 package ru.ryazanova.itbookstore.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.ryazanova.itbookstore.entity.Product;
 
 @Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+   /* Spring exposes automatically the endpoint
+    http://localhost:8080/api/products/search/findByCategoryId?id=2
+    */
+    Page<Product> findByCategoryId(@RequestParam("id") Integer id, Pageable pageable);
 }
